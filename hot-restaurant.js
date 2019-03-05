17ae809a011fced830e6672fb556388ce30c1c01
Yagmur Guzeldereli <ms.guzeldereli@gmail.com>
2019-03-04 18:55:42 -0600

    its a start2

---
+++ hot-restaurant.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.get('/', function (req, res) {
+    console.log('home page');
+    res.sendFile(path.join("/index.html"));
+});
+app.get('/reserve', function (req, res) {
+    console.log('reserve page');
+    res.sendFile(path.join('/make.html'));
+    });
+
+
+var tables = [
+    {name: "Yagmur",
+    phone: "112782743",
+    email: "yagmur@mail.com",
+    customerID: 1},
+    {
+        name: "mike",
+    phone: "112782744",
+    email: "mike@mail.com",
+    customerID: 2
+
+    }]
